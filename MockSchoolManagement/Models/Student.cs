16ab72e4f1aaa4bfa5045ecf38cac3e5d47d8f41
powerbear0083 <powerbear0083@gmail.com>
2021-04-06 21:30:44 +0800

    新增一個 StudentCreateViewModel 將 Student 底下的驗證條件搬過去

--- MockSchoolManagement/Models/Student.cs
+++ MockSchoolManagement/Models/Student.cs
@@ -11,29 +11,15 @@ namespace MockSchoolManagement.Models
     {
         public int Id { get; set; }
         
-        [Display(Name = "名字")]
-        [
-            Required(ErrorMessage = "請輸入名字，不能為空"),
-            MaxLength(50, ErrorMessage = "名字字數不能超過 50 個字")
-        ]
         public string Name { get; set; }
         
         //public string Major { get; set; }
         // 將 Major 型別設置為 MajorEnum
         // MajorEnum? 使 Major 屬性值可為空
         
-        [Required(ErrorMessage = ("請選擇一門主修"))]
-        [Display(Name = "主修")]
         public MajorEnum? Major { get; set; }
         
-        [Display(Name = "Email")]
-        [
-            RegularExpression(
-                @"^[a-zA-z0-9_.+-]+@[a-zA-z0-9-]+\.[a-zA-z0-9-.]+$",
-                ErrorMessage = "Email 格式不正確"
-                )
-        ]
-        [Required(ErrorMessage = "請輸入 Email，不能為空")]
+        
         public string Email { get; set; }
 
         public string PhotoPath { get; set; }
---
+++ MockSchoolManagement/ViewModels/StudentCreateViewModel.cs
@@ -0,0 +1,38 @@
+﻿using MockSchoolManagement.Models.EnumTypes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MockSchoolManagement.ViewModels
+{
+    public class StudentCreateViewModel
+    {
+
+        [
+            Required(ErrorMessage = "請輸入名字，不能為空"),
+            MaxLength(50, ErrorMessage = "名字字數不能超過 50 個字")
+        ]
+        [Display(Name = "名字")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = ("請選擇一門主修"))]
+        [Display(Name = "主修")]
+        public MajorEnum?Major { get; set; }
+        
+        [Display(Name = "Email")]
+        [
+            RegularExpression(
+                @"^[a-zA-z0-9_.+-]+@[a-zA-z0-9-]+\.[a-zA-z0-9-.]+$",
+                ErrorMessage = "Email 格式不正確"
+            )
+        ]
+        [Required(ErrorMessage = "請輸入 Email，不能為空")]
+        public string Email { get; set; }
+        
+        [Display(Name = "頭像")]
+        public IFormFile Photo { get; set; }
+    }
+}
