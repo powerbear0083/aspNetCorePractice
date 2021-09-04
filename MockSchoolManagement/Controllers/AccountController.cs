668f31f72d501527144678009d10163f42cdba38
yinhsun <powerbear0083@gmail.com>
2021-09-04 21:05:50 +0800

    增加 account 頁面 controller view viewModel

---
+++ MockSchoolManagement/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MockSchoolManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ MockSchoolManagement/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MockSchoolManagement.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "電子郵件")]
+        public  string Email { get; set; }
+       
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "密碼")]
+        public string Password { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "確認密碼")]
+        [Compare("Password", ErrorMessage = "密碼與確認密碼不一致，請重新輸入")]
+        public string ConfirmPassword { get; set; }
+        
+    }
+}
